957937c6c38bcceaa71f50d120660bc64e6bb312
Haruka Ichinose <hic21@uclive.ac.nz>
2022-07-08 03:04:08 +1200

    Refactored for firebase version 8 connecting to a different Firebase project.

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,18 +4,15 @@ import ImgSlider from "./ImgSlider";
 import Viewers from "./Viewers";
 import Movies from "./Movies";
 import db from "../firebase";
-import { collection, query, getDocs } from "firebase/firestore";
 
 const Home = () => {
   useEffect(() => {
-    async function getMovies() {
-      const q = query(collection(db, "movies"));
-      const querySnapshot = await getDocs(q);
-      querySnapshot.forEach((snapshot) => {
-        console.log(snapshot);
-      });
-    }
-    getMovies();
+    db.collection("movies").onSnapshot((snapshot) => {
+      let tempMovies = snapshot.docs.map((doc) => {
+        console.log(doc.data());
+        return { id: doc.id, ...doc.data() }
+      })
+    })
   }, []);
 
   return (
--- src/firebase.js
+++ src/firebase.js
@@ -1,51 +1,20 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
-import { getAuth, GoogleAuthProvider } from 'firebase/auth'
-import { getStorage } from 'firebase/storage';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import firebase from "firebase";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCUWwlvUmxD9P1cq78Q3x1qrLDw7zIUtl0",
-  authDomain: "disney-plus-clone-c290c.firebaseapp.com",
-  projectId: "disney-plus-clone-c290c",
-  storageBucket: "disney-plus-clone-c290c.appspot.com",
-  messagingSenderId: "289701551627",
-  appId: "1:289701551627:web:9c346352f5d9223b01f10e"
+  apiKey: "AIzaSyBOhSiJwc_Wmf9QbeHhHFFRS7fWjID_E08",
+  authDomain: "disney-plus-clone-640bb.firebaseapp.com",
+  projectId: "disney-plus-clone-640bb",
+  storageBucket: "disney-plus-clone-640bb.appspot.com",
+  messagingSenderId: "1072582347227",
+  appId: "1:1072582347227:web:2b66a662d6ffd6ba8b7ffe",
+  measurementId: "G-W3PLXGZ2V3",
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const auth = getAuth();
-const provider = new GoogleAuthProvider();
-const storage = getStorage();
-
-console.log("HERE");
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+const storage = firebase.storage();
 
 export { auth, provider, storage };
 export default db;
-
-
-// import firebase from "firebase";
-
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAUXp0z8x4PA1cZV7StnTG8HGvEsxomp3U",
-//   authDomain: "inspired-fact-200600.firebaseapp.com",
-//   projectId: "inspired-fact-200600",
-//   storageBucket: "inspired-fact-200600.appspot.com",
-//   messagingSenderId: "376536471085",
-//   appId: "1:376536471085:web:bf3788a41cffd1137c8aa4",
-//   measurementId: "G-WEYB0EWMKY"
-// };
-
-// const firebaseApp = firebase.initializeApp(firebaseConfig);
-// const db = firebaseApp.firestore();
-// const auth = firebase.auth();
-// const provider = new firebase.auth.GoogleAuthProvider();
-// const storage = firebase.storage();
-
-// export { auth, provider, storage };
-// export default db;
