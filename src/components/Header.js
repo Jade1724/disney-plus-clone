e8c147911420f3ad17d4b9a002615ef202fc20c1
Jade1724 <haruka.i1724@gmail.com>
2022-07-13 02:36:40 +1200

    Created userSlice and Login handler in Header component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,42 +1,69 @@
-import React from 'react'
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
+import { selectUserName, selectUserPhoto, setUserLogin } from "../features/user/userSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { auth, provider } from "../firebase";
 
 const Header = () => {
+    const dispatch = useDispatch();
+  const userName = useSelector(selectUserName);
+  const userPhoto = useSelector(selectUserPhoto);
+
+  const signIn = () => {
+    auth.signInWithPopup(provider)
+    .then((result) => {
+        let user = result.user
+      dispatch(setUserLogin({
+        name: user.displayName,
+        email: user.email,
+        photo: user.photoURL
+      }))
+    })
+  }
+
   return (
     <Nav>
-        <Logo src="/images/logo.svg" />
-        <NavMenu>
+      <Logo src="/images/logo.svg" />
+      {!userName ? (
+        <LoginContainer>
+          <Login onClick={signIn}>Login</Login>
+        </LoginContainer>
+      ) : (
+        <>
+          <NavMenu>
             <a>
-                <img src='/images/home-icon.svg' />
-                <span>HOME</span>
+              <img src="/images/home-icon.svg" />
+              <span>HOME</span>
             </a>
             <a>
-                <img src='/images/search-icon.svg' />
-                <span>SEARCH</span>
+              <img src="/images/search-icon.svg" />
+              <span>SEARCH</span>
             </a>
             <a>
-                <img src='/images/watchlist-icon.svg' />
-                <span>WATCHLIST</span>
+              <img src="/images/watchlist-icon.svg" />
+              <span>WATCHLIST</span>
             </a>
             <a>
-                <img src='/images/original-icon.svg' />
-                <span>ORIGINAL</span>
+              <img src="/images/original-icon.svg" />
+              <span>ORIGINAL</span>
             </a>
             <a>
-                <img src='/images/movie-icon.svg' />
-                <span>MOVIES</span>
+              <img src="/images/movie-icon.svg" />
+              <span>MOVIES</span>
             </a>
             <a>
-                <img src='/images/series-icon.svg' />
-                <span>SERIES</span>
+              <img src="/images/series-icon.svg" />
+              <span>SERIES</span>
             </a>
-        </NavMenu>
-        <UserImg src='https://content.fortune.com/wp-content/uploads/2022/06/Elon-Musk-return-to-office-policy-GettyImages-1395059297.jpg?resize=750,500'/>
+          </NavMenu>
+          <UserImg src="https://content.fortune.com/wp-content/uploads/2022/06/Elon-Musk-return-to-office-policy-GettyImages-1395059297.jpg?resize=750,500" />
+        </>
+      )}
     </Nav>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
 
 const Nav = styled.div`
   height: 70px;
@@ -45,11 +72,11 @@ const Nav = styled.div`
   align-items: center;
   padding: 0 36px;
   overflow-x: hidden;
-`
+`;
 
 const Logo = styled.img`
   width: 80px;
-`
+`;
 
 const NavMenu = styled.div`
   display: flex;
@@ -64,40 +91,64 @@ const NavMenu = styled.div`
     cursor: pointer;
 
     img {
-        height: 20px;
+      height: 20px;
     }
     span {
-        font-size: 13px;
-        letter-spacing: 1.4px;
-        position: relative;
+      font-size: 13px;
+      letter-spacing: 1.4px;
+      position: relative;
 
-        &:after {
-            content: "";
-            height: 2px;
-            background: white;
-            position: absolute;
-            left: 0;
-            right: 0;
-            bottom: -6px;
-            opacity: 0;
-            transform-origin: left center;
-            transition: all 250ms cubic-bezier(0.25, 0.46, 0.46, 0.94) 0s;
-            transform: scaleX(0);
-        }
+      &:after {
+        content: "";
+        height: 2px;
+        background: white;
+        position: absolute;
+        left: 0;
+        right: 0;
+        bottom: -6px;
+        opacity: 0;
+        transform-origin: left center;
+        transition: all 250ms cubic-bezier(0.25, 0.46, 0.46, 0.94) 0s;
+        transform: scaleX(0);
+      }
     }
 
     &:hover {
-        span:after {
-            transform: scaleX(1);
-            opacity: 1;
-        }
+      span:after {
+        transform: scaleX(1);
+        opacity: 1;
+      }
     }
   }
-`
+`;
 
 const UserImg = styled.img`
-    width: 48px;
-    height: 48px;
-    border-radius: 50px;
-    cursor: pointer;
+  width: 48px;
+  height: 48px;
+  border-radius: 50px;
+  cursor: pointer;
+`;
+
+const Login = styled.div`
+  border: 1px solid #f9f9f9;
+  padding: 8px 16px;
+  border-redius: 4px;
+  letter-spacing: 1.5px;
+  text-transform: uppercase;
+  background-color: rgba(0, 0, 0, 0.6);
+  transition: all 0.2s ease 0s;
+  cursor: pointer;
+
+  &:hover {
+    background-color: #f9f9f9;
+    color: #000;
+    border-color: transparent;
+  }
+`;
+
+const LoginContainer = styled.div`
+  flex: 1;
+  display: flex;
+  justify-content: flex-end;
+
 `
